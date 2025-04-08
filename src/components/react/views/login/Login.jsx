6dbdab7dc6bf734bb2efe7231cb4bef445bfae0f
sidonay499 <git config --global user.email>
2025-04-08 10:54:04 -0300

    complete login layout

--- src/components/react/views/login/Login.jsx
+++ src/components/react/views/login/Login.jsx
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom'
 import LoginForm from '../../forms/loginForm/LoginForm'
 import './login.css'
 
 const Login = ()=>{
     return(
-        <section>
+        <section className='container_login' >
+            <h2>INGRESO ADMIN</h2>
             <LoginForm/>
+            <div className='box_redirect' >
+                <Link to={'/admin/signin'} >
+                    <p className='text_redirect' >REGISTRARSE</p>
+                </Link>
+            </div>
         </section>
     )
 }
