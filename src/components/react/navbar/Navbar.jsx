88b91206c3cc11ebbff40d8afa438bcf704e7f20
sidonay499 <git config --global user.email>
2025-04-10 11:37:48 -0300

    navbar has been changed

---
+++ src/components/react/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './navbar.css'
+
+const Navbar = ({path})=>{
+    console.log(path)
+    return(
+        <section className='aside_navbar' >
+            {path != '/' && 
+            <div className='box_navbar' >
+                <a href="">SOPORTE</a>
+                <a href="/">
+                    <img
+                        className='image_navbar'
+                        style={{viewTransitionName:'asmodeo'}}
+                        src="./logo.svg" 
+                        alt=""
+                    />
+                </a>
+                <a href="">UNIRTE</a>
+            </div>
+            }
+        </section>
+    )
+}
+
+export default Navbar
