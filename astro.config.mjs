aff2903b1ad09b1a6faf9f4afe1401f0f9eef7f2
sidonay499 <git config --global user.email>
2025-05-09 18:38:25 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations:[react()],
     output:'server',
-    adapter:vercel()
+    adapter:vercel(),
+    integrations:[react()]
 });
