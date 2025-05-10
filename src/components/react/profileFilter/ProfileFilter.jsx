af50680865dc2d654a733b534aa20760145f8cc9
sidonay499 <git config --global user.email>
2025-05-10 11:43:30 -0300

    profileFilter created

---
+++ src/components/react/profileFilter/ProfileFilter.jsx
@@ -0,0 +1,15 @@
+import './profileFilter.css'
+
+const ProfileFilter = ()=>{
+    return(
+        <aside className='aside_filter' >
+            <div className='box_filter' >
+                <div className='box_gender' >
+
+                </div>
+            </div>
+        </aside>
+    )
+}
+
+export default ProfileFilter
