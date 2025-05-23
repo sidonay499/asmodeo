63415448dd331a0a1c1cb10ae0fd77178518b42a
sidonay499 <git config --global user.email>
2025-05-23 11:31:49 -0300

    Fixed filter and applied dimensions for mobile

--- src/components/react/cardsPreview/CardsPreview.jsx
+++ src/components/react/cardsPreview/CardsPreview.jsx
@@ -11,13 +11,128 @@ const CardsPreview = ()=>{
     const [detail,serDetail] = useState('')
     const [page,setPage] = useState(1)
     const {
-        escorts,
+ 
         filtered,
         loading,
         errors,
         getEscorts,
         cleanErrors,
     } = useStore()
+
+    const escorts =  [
+  {
+    id: 1,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 150,
+    name: "Camila",
+    age: 25,
+    location: "Buenos Aires"
+  },
+  {
+    id: 2,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 200,
+    name: "Sofia",
+    age: 28,
+    location: "Córdoba"
+  },
+  {
+    id: 3,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 180,
+    name: "Lucía",
+    age: 23,
+    location: "Rosario"
+  },
+  {
+    id: 4,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 220,
+    name: "Valentina",
+    age: 30,
+    location: "Mendoza"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  },
+  {
+    id: 5,
+    imageProfile: "https://media.vogue.es/photos/5cc7516315d9a34a16e8381b/master/w_1600%2Cc_limit/modelos_nuevas_new_york_fashion_week_shan_fernandez_5573.jpg",
+    price: 170,
+    name: "Julieta",
+    age: 26,
+    location: "Salta"
+  }
+];
 
     useEffect(()=>{
         if(escorts.length === 0){
--- src/components/react/profileFilter/ProfileFilter.jsx
+++ src/components/react/profileFilter/ProfileFilter.jsx
@@ -18,56 +18,60 @@ const ProfileFilter = ()=>{
     return(
         <aside className='aside_filter' >
             <div className='box_filter' >
-                <div onClick={()=>cleanFilter} className='box_all' >
-                    <span>ALL</span>
-                </div>
-                <div className='box_gender' >
-                    <span className={`item item_${position}`} />
-                    <div
-                        onClick={()=>{
-                                setPosition('rigth')
-                                searchToParams('gender','MALE')
-                            }}
-                        className='item_filter' 
-                    >                        
-                        <p>HOMBRE</p>
+                <div className='boxes_fil' >
+                    <div onClick={()=>cleanFilter} className='box_all' >
+                        <span>ALL</span>
                     </div>
-                    <div 
-                        onClick={()=>{
-                            setPosition('left')
-                            searchToParams('gender','FEMALE')
-                        }} 
-                        className='item_filter' 
-                    >                        
-                        <p>MUJER</p>
+                    <div className='box_gender' >
+                        <span className={`item item_${position}`} />
+                        <div
+                            onClick={()=>{
+                                    setPosition('rigth')
+                                    searchToParams('gender','MALE')
+                                }}
+                            className='item_filter' 
+                        >                        
+                            <p>HOMBRE</p>
+                        </div>
+                        <div 
+                            onClick={()=>{
+                                setPosition('left')
+                                searchToParams('gender','FEMALE')
+                            }} 
+                            className='item_filter' 
+                        >                        
+                            <p>MUJER</p>
+                        </div>
+                    </div>
+                    <div className="options_filter" >
+                        <AgeFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <LocationFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <StateFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <CountryFilter/>
                     </div>
                 </div>
-                <div className="options_filter" >
-                    <AgeFilter/>
-                </div>
-                <div className="options_filter" >
-                    <LocationFilter/>
-                </div>
-                <div className="options_filter" >
-                    <StateFilter/>
-                </div>
-                <div className="options_filter" >
-                    <CountryFilter/>
-                </div>
-                <div className="options_filter" >
-                    <HairFilter/>
-                </div>
-                <div className="options_filter" >
-                    <EyesColor/>
-                </div>
-                <div className="options_filter" >
-                    <ShavedFilter/>
-                </div>
-                <div className="options_filter" >
-                    <CategoryFilter/>
-                </div>
-                <div className="options_filter" >
-                    <BiotypeFilter/>
+                <div className='boxes_fil' >
+                    <div className="options_filter" >
+                        <HairFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <EyesColor/>
+                    </div>
+                    <div className="options_filter" >
+                        <ShavedFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <CategoryFilter/>
+                    </div>
+                    <div className="options_filter" >
+                        <BiotypeFilter/>
+                    </div>
                 </div>
             </div>
         </aside>
