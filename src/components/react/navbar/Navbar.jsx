82184520206975ed0e87ea951bcc46882354a4e3
sidonay499 <git config --global user.email>
2025-05-26 18:23:10 -0300

    page blog has been created

--- src/components/react/navbar/Navbar.jsx
+++ src/components/react/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({path})=>{
         <section className='aside_navbar' >
             {path != '/' && 
             <div className='box_navbar' >
-                <a href="">BLOG</a>
+                <a href="/blog">BLOG</a>
                 <a href="/">
                     <img
                         className='image_navbar'
