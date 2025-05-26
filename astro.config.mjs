12b8b7701f79c96317b54d570dae0cdcccf4507c
sidonay499 <git config --global user.email>
2025-05-26 17:57:31 -0300

    create sitemap asmodeo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site:'https://asmodeo.net',
     adapter:vercel(),
     output:'static',
-    integrations:[react()]
+    integrations:[
+        react(), 
+        sitemap({
+            lastmod: new Date(),
+            priority:1
+        })
+    ]
 });
