704653cbb521a03660fba526e2a5c5f61462cf28
sidonay499 <jona_1969@hotmail.com>
2025-07-17 10:10:11 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react"
 import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap';
-import getAllEscorts from './src/adapters/escorts/getAllEscorts';
+
 
 export default defineConfig ({
   site: 'https://asmodeo.net',
@@ -13,17 +13,8 @@ export default defineConfig ({
     react(),
     vercel(),
     sitemap({
-      serialize: async (item)=>{
-        const data = await getAllEscorts()
-        data.escorts.map(escort => {
-          return {
-            url: `https://asmode.net/${escort.name}/${escort.id}`,
-            lastmod: new Date(escort.updatedAt).toISOString(),
-            changefreq: 'weekly',
-            priority: 0.8,
-          }
-        })
-      }
-    }),
+      lastmod:new Date(),
+      priority:1
+    })
   ]
 })
