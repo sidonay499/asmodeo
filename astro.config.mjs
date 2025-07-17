a859334e9322a760e72d3a01f4a780fde98ec1b8
sidonay499 <jona_1969@hotmail.com>
2025-07-17 13:01:43 -0300

    new config of sitemap, url map

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
+import axios from 'axios';
+
+const pages = async () => {
+  const res = await axios.get('https://asmodeo-back.onrender.com/escort')
+  const customPages = res.data.escorts.map((escort) => {
+    return `https://asmodeo.net/${escort.name}/${escort.id}`
+  });
+  return customPages;
+}
+
+export default defineConfig({
+  site: 'https://asmodeo.net',
+  output: 'server',
+  adapter: vercel(),
+  integrations: [
+    react(),
+    sitemap({
+      customPages:await pages(),
+      changefreq: 'weekly',
+      priority: 0.9,
+    }),
+  ],
+});
